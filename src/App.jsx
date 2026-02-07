dd040df8026c4382f7ddfda1a605896f6425c652
Antony Mwangi <antonymwangiw85@gmail.com>
2026-02-07 19:35:36 +0300

    search engine optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -9,14 +11,26 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <HelmetProvider>
+      <div>
+        <Helmet>
+          <title>Antony Mwangi Portfolio</title>
+          <meta
+            name="description"
+            content="Portfolio website of Antony Mwangi showcasing web development projects built with React, Django, and modern web technologies."
+          />
+          <meta name="keywords" content="Antony Mwangi, React Developer, Web Developer, Portfolio, Django, JavaScript" />
+          <meta name="author" content="Antony Mwangi" />
+        </Helmet>
+
+        <Navbar />
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </HelmetProvider>
   );
 }
 
